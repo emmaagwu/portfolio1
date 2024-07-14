1b382c3246f452a62608038edc7b6e9284cac773
Agwu Emmanuel <emmanuelagwu89@gmail.com>
2024-07-14 17:56:09 +0100

    Update text color in TextGenerateEffect component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,13 +40,13 @@ const Hero = () => {
 
           <TextGenerateEffect 
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Agwu Emmanuel" 
+            words="AGWU EMMANUEL" 
           />
           <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
           Fullstack Software Engineer
           </p>
 
-          <a href='#about'>
+          <a href='#projects'>
             <MagicButton title="Show my work"
             icon={<FaLocationArrow />}
             position='right'
